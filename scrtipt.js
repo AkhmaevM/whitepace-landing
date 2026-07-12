02e4b33d4fc7ac0feab8ae7af6c65c182e3106b5
AkhmaevM <m.axmaev@yandex.ru>
2026-07-12 15:18:43 +0300

    Created styles for reviews section

--- scrtipt.js
+++ scrtipt.js
@@ -1,15 +1,30 @@
-const sectionImg = document.querySelectorAll('.section__img')
-const sectionBtn = document.querySelectorAll('.section__btn')
+const sectionImg = document.querySelectorAll('.section__img');
+const sectionBtn = document.querySelectorAll('.section__btn');
+const reviewCard = document.querySelectorAll('.reviews__card');
+const reviewQuote = document.querySelector('.reviews__icon');
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
 	sectionImg.forEach(image => {
-		image.src = '/images/Image-container.svg'
+		image.src = '/images/Image-container.svg';
 	})
 
 	sectionBtn.forEach(button => {
 		const btnArrow = document.createElement('img')
 		btnArrow.src = '/images/icons/white arrow.svg';
-        btnArrow.className = 'btn__arrow';
-		button.appendChild(btnArrow)
+		btnArrow.className = 'btn__arrow';
+		button.appendChild(btnArrow);
 	})
+
+
+	reviewCard.forEach(card => {
+		card.addEventListener('mouseover', () => {
+			reviewQuote.src = '/images/icons/Quote.svg';
+		})
+	})
+	// reviewCard.addEventListener('onMouse', () => {
+	// 	reviewQuote.src = '/images/icons/Quote.svg';
+	// })
+
 })
